2581e709d9384b99909227b5ade893868a990cf2
Squarenoize <odiotech@yahoo.ca>
2026-07-29 12:50:16 +0200

    removing ux-turbo dependencie - registration form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\RegistrationType;
+use App\Entity\User;
+use App\Entity\Worker;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
@@ -12,7 +18,44 @@ final class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('public/dispatch.html.twig', [
-            'pageTitle' => 'Bienvenue',
+            'pageTitle' => 'Bienvenue sur TaskLinker',
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('public/login.html.twig', [
+            'pageTitle' => 'Se connecter',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $worker = new Worker();
+        $worker->setContract('CDI');
+        $worker->setStartingDate(new \DateTimeImmutable());
+        $user->setWorker($worker);
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('public/register.html.twig', [
+            'pageTitle' => 'S\'inscrire',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/WorkerRegistrationType.php
+++ src/Form/WorkerRegistrationType.php
@@ -2,29 +2,28 @@
 
 namespace App\Form;
 
+use App\Entity\Project;
 use App\Entity\User;
 use App\Entity\Worker;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
-class UserType extends AbstractType
+class WorkerRegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'required' => true
-            ])
+            ->add('lastname')
+            ->add('firstname')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Worker::class,
         ]);
     }
 }
